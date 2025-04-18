66c694223d31645e7c7f0fdeea48ab39cd717d83
Mounir Samite <mounir.samite@gmail.com>
2025-04-18 20:06:19 +0200

    add fill function to Streams TDD

--- src/test/scala/u03/StreamTest.scala
+++ src/test/scala/u03/StreamTest.scala
@@ -27,4 +27,9 @@ class StreamTest:
     val str1 = Stream.iterate(0)(_ + 1) // {0,1,2,3,..}
     val str2 = Stream.takeWhile(str1)(_ < 5) // {0,1,2,3,4}
     assertEquals(Cons(0, Cons(1, Cons(2, Cons(3, Cons(4, Nil()))))), Stream.toList(str2))
+
+  @Test def testFill(): Unit =
+    assertEquals(Stream.toList(Stream.fill(3)("a")), Cons("a", Cons("a", Cons("a", Nil()))))
+    assertEquals(Stream.toList(Stream.fill(0)("a")), Nil())
+
 end StreamTest
